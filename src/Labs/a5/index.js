a221c094d50b1bfe9e59c96badedaa61cc842971
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2023-11-09 16:46:10 -0500

    Completed 3.1.1 of Lab

---
+++ src/Labs/a5/index.js
@@ -0,0 +1,17 @@
+import EncodingParametersInURLS from "./EncodingParametersInURLS";
+
+function Assignment5() {
+    return (
+        <div>
+            <h1>Assignment 5</h1>
+            <div className={"list-group"}>
+                <a href={"http://localhost:4000/a5/welcome"}
+                   className={"list-group-item"}>
+                    Welcome
+                </a>
+                <EncodingParametersInURLS/>
+            </div>
+        </div>
+    );
+}
+export default Assignment5;
